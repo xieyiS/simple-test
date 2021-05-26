df37e03380dfeb3ca0df8b5578ada9848043dc1f
sunxieyi <540416364@qq.com>
2021-05-26 17:36:27 +0800

    增加TestSerializableCopy测试类,以测试通过实现Serializable接口，通过对象的序列化和反序列化实现克隆，实现真正的深度克隆

---
+++ src/main/java/TestSerializableCopy.java
@@ -0,0 +1,41 @@
+import entity.Car;
+import entity.Person;
+
+import java.io.*;
+
+/*如何实现对象克隆？
+有两种方式：
+1). 实现Cloneable接口并重写Object类中的clone()方法；
+2). 实现Serializable接口，通过对象的序列化和反序列化实现克隆，可以实现真正的深度克隆，代码如下：
+*/
+public class TestSerializableCopy {
+    @SuppressWarnings("unchecked")
+    public static <T extends Serializable> T clone(T obj) throws Exception {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bout);
+        oos.writeObject(obj);
+
+        ByteArrayInputStream bin = new ByteArrayInputStream(bout.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bin);
+        return (T) ois.readObject();
+
+        // 说明：调用ByteArrayInputStream或ByteArrayOutputStream对象的close方法没有任何意义
+        // 这两个基于内存的流只要垃圾回收器清理对象就能够释放资源，这一点不同于对外部资源（如文件流）的释放
+    }
+
+    public static void main(String[] args) {
+        try {
+            Person p1 = new Person("郭靖", 33, new Car("Benz", 300));
+            Person p2 = clone(p1);   // 深度克隆
+            p2.getCar().setBrand("BYD");
+            // 修改克隆的Person对象p2关联的汽车对象的品牌属性
+            // 原来的Person对象p1关联的汽车不会受到任何影响
+            // 因为在克隆Person对象时其关联的汽车对象也被克隆了
+            System.out.println("p1=" + p1);
+            System.out.println("p2=" + p2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
